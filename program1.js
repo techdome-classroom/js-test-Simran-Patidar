440b6879b18e1592a9d1e13dc3df16d9098c0e96
Simran Patidar <simranpatidar187@gmail.com>
2024-07-26 13:17:09 +0530

    solved

--- program1.js
+++ program1.js
@@ -1,8 +1,53 @@
-const getTotalIsles = function (grid) {
+const direction = [
+  [-1, 0],
+  [1, 0],
+  [0, -1],
+  [0, 1],
+];
 
+const solve = function (grid, i, j) {
+  grid[i][j] = '$';
 
-  // write your code here
+  for (let k = 0; k < 4; k++) {
+    let new_i = i + direction[k][0];
+    let new_j = j + direction[k][1];
 
+    if (
+      new_i >= 0 &&
+      new_i < grid.length &&
+      new_j >= 0 &&
+      new_j < grid[0].length &&
+      grid[new_i][new_j] == 'L'
+    ) {
+      solve(grid, new_i, new_j);
+    }
+  }
 };
 
+const getTotalIsles = function (grid) {
+  // write your code here
+  let m = grid.length;
+  let n = grid[0].length;
+
+  let count = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      if (grid[i][j] == "L") {
+        count++;
+        solve(grid, i, j);
+      }
+    }
+  }
+
+  return count;
+};
+
+let grid= [
+  ["L","L","W","W","W"],
+  ["L","L","W","W","W"],
+  ["W","W","L","W","W"],
+  ["W","W","W","L","L"],
+]
+
+console.log(getTotalIsles(grid));
 module.exports = getTotalIsles;
--- program2.js
+++ program2.js
@@ -1,7 +1,50 @@
 const decodeTheRing = function (s, p) {
+  // write your code here
 
-    // write your code here
+  let msgLen = s.length;
+  let keyLen = p.length;
+  let i = 0;
+  let j = 0;
+  let starIdx = -1;
+  let match = 0;
 
-  };
-  
-  module.exports = decodeTheRing;
+  while (i < msgLen) {
+    //char matched or p has ?
+    if (j < keyLen && (p[j] === "?" || p[j] === s[i])) {
+      i++;
+      j++;
+    }
+
+    //star char in p
+    else if (j < keyLen && p[j] === "*") {
+      starIdx = j;
+      match = i;
+      j++;
+    }
+
+    //char is not matched and is not ? -> use star char
+    else if (starIdx != -1) {
+      j = starIdx + 1;
+      match++;
+      i = match;
+    }
+
+    //matched
+    else {
+      return false;
+    }
+  }
+
+
+  //check p string for star char only
+  if (j < keyLen && p[j] === "*") {
+    return j++;
+  }
+
+  if (j === keyLen) return true;
+  else return false;
+};
+
+// console.log(decodeTheRing("hello", "h*llo"));
+
+module.exports = decodeTheRing;
